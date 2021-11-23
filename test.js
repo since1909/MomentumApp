04e31529ea4f664ee863f72e741e64dfe9eee0f8
jiwon <since1909@daum.net>
2021-11-23 14:36:00 +0900

    [Modify] :  이벤트 리스너 테스트

--- test.js
+++ test.js
@@ -66,6 +66,8 @@ const h1 = document.querySelector(".hello h1");
 
 h1.innerText = "Hello!";
 
+console.dir(h1);
+
 // event listener
 
 function handleTitleClick() {
@@ -73,4 +75,36 @@ function handleTitleClick() {
     console.log("title was clicked");
 }
 
+function handleMouseEnter(){
+    h1.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave(){
+    h1.innerText = "Mouse is gone!"
+}
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+    alert("copier!!");
+}
+
+function handleWindowOffline() {
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+    alert("All GOOD");
+}
+
 h1.addEventListener("click", handleTitleClick);
+//h1.onclick = handleTitleClick;
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
