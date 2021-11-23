755c9cfec5fd055971e5be2507eb6241d446cb05
jiwon <since1909@daum.net>
2021-11-23 17:00:03 +0900

    [Modify] : Use localStorage

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     //submit 이벤트를 통해 브라우저가 디폴트로 실행하는 행동을 막는 함수
@@ -10,8 +11,21 @@ function onLoginSubmit(event){
     loginForm.classList.add(HIDDEN_CLASSNAME);
     
     const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+
+    paintGreeting(username);
+}
+
+function paintGreeting(username) {
     greeting.innerText = `Hello ${username}`
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreeting(savedUsername);
+}
