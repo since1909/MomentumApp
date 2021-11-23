b1206f5e4c0dacbbc93ac8fcf3698b4c2f65f747
jiwon <since1909@daum.net>
2021-11-23 16:25:07 +0900

    [Modify] : make form hidden & greet to user

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){
     //submit 이벤트를 통해 브라우저가 디폴트로 실행하는 행동을 막는 함수
     event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    
     const username = loginInput.value;
-    console.log(username);
+    greeting.innerText = `Hello ${username}`
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
