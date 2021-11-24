970a13602e1f60fa09d039ea16ed6983fe7e56cf
jiwon <since1909@daum.net>
2021-11-24 13:14:43 +0900

    [Modify] : ToDo List 삭제 기능 추가

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,35 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+    toDos = toDos.filter(toDo => toDo.id != parseInt(li.id));
+    saveToDos();
+    
+}
+
 function paintToDo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
+
     const span = document.createElement("span");
+    span.innerText = newTodo.text;
+    
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo)
 
     li.appendChild(span);
-    span.innerText = newTodo;
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
@@ -15,7 +38,22 @@ function handelToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    paintToDo(newTodo);
+    const newTodoObj = {
+        id : Date.now(),
+        text : newTodo
+    }
+
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handelToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos != null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
